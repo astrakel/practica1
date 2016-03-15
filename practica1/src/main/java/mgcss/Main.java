715f64ef3f88f5bd683976ba34397ce41b18aaf0
astrakel <kitiplas2002@hotmail.com>
2016-03-15 16:47:36 +0100

    añado una linea

--- practica1/src/main/java/mgcss/Main.java
+++ practica1/src/main/java/mgcss/Main.java
@@ -7,6 +7,7 @@ package mgcss;
 public class Main {
     public static void main (String[] args){
       System.out.println("Bienvenidos a las practicas de MGCSS");
+      System.out.println("Voy a añadir esta linea");
       for (int i=0; i<10;i++){
          System.out.print(i+",");
       }
